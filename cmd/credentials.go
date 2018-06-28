79184fb9c503a25ebddb2639653f3589bd2cd310
puneet <puneet@platform9.com>
2018-06-28 11:57:59 -0700

    Change create credentials to take location of private key file. And to write base64 encoded contents in the state file.

--- cmd/credentials.go
+++ cmd/credentials.go
@@ -1,27 +1,32 @@
 package cmd
 
 import (
+	"io/ioutil"
+	"log"
+
+	"encoding/base64"
+
 	"github.com/platform9/pf9-clusteradm/common"
 	"github.com/platform9/pf9-clusteradm/statefileutil"
 	"github.com/spf13/cobra"
-	"log"
 )
 
 var credentialsCmdCreate = &cobra.Command{
 	Use:   "credentials",
 	Short: "Create new SSH credentials",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		bytes, err := ioutil.ReadFile(cmd.Flag("privateKey").Value.String())
+		if err != nil {
+			log.Fatalf("Failed to read key file with err %v\n", err)
+		}
 		sshSecret := common.SSHSecret{
 			User:       cmd.Flag("user").Value.String(),
-			PrivateKey: cmd.Flag("privateKey").Value.String(),
+			PrivateKey: base64.StdEncoding.EncodeToString(bytes),
 		}
-
 		cs, err := statefileutil.ReadStateFile()
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		cs.SSHSecret = sshSecret
 		statefileutil.WriteStateFile(&cs)
 	},
@@ -30,6 +35,6 @@ var credentialsCmdCreate = &cobra.Command{
 func init() {
 	createCmd.AddCommand(credentialsCmdCreate)
 	credentialsCmdCreate.Flags().String("user", "root", "SSH username")
-	credentialsCmdCreate.Flags().String("privateKey", "", "SSH privateKey")
+	credentialsCmdCreate.Flags().String("privateKey", "", "SSH privateKey file location")
 	credentialsCmdCreate.MarkFlagRequired("privateKey")
 }
