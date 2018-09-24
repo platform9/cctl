f7117a889fc5370ac7620df4b33ecb90490b0a85
Vann Teves <vann@platform9.com>
2018-09-24 16:09:45 -0700

    refactor unit tests

--- pkg/migrate/migrate_v1_test.go
+++ pkg/migrate/migrate_v1_test.go
@@ -8,48 +8,72 @@ import (
 	"testing"
 )
 
+func marshal(s StateV0toV1) []byte {
+	stateBytes, err := yaml.Marshal(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return stateBytes
+}
+
+func decode(b []byte) {
+	newState := util.DecodeMigratedState(b)
+	if newState.SchemaVersion != 1 {
+		log.Fatal("Migration failed.")
+	}
+}
+
 // MigrateV0toV1 adds a schemaVersion field to the state file
 func TestMigrateV0toV1(t *testing.T) {
 	testSchemaVersionUpdate(t)
-	testSchemaVersionCannotUpdate(t)
+	testHigherSchemaVersion(t)
+	testSameSchemaVersion(t)
 }
 
 func testSchemaVersionUpdate(t *testing.T) {
-	t.Run("Schema Version updated to 1", func(t *testing.T) {
+	t.Run("Update schemaVersion to 1", func(t *testing.T) {
 		testYaml := StateV0toV1{
 			SchemaVersion: 0,
 		}
-		stateBytes, err := yaml.Marshal(testYaml)
-		if err != nil {
-			log.Fatal(err)
-		}
+		stateBytes := marshal(testYaml)
 
 		migratedBytes, err := MigrateV0toV1(&stateBytes)
 		if err != nil {
 			log.Fatal(err)
 		}
-		newState := util.DecodeMigratedState(migratedBytes)
-		if newState.SchemaVersion != 1 {
-			log.Fatal("Migration failed.")
-		}
+
+		decode(migratedBytes)
 	})
 }
 
-func testSchemaVersionCannotUpdate(t *testing.T) {
-	t.Run("Schema Version updated to 1", func(t *testing.T) {
+func testHigherSchemaVersion(t *testing.T) {
+	t.Run("Cannot update schemaVersion > 1", func(t *testing.T) {
 		testYaml := StateV0toV1{
 			SchemaVersion: 5,
 		}
-		stateBytes, err := yaml.Marshal(testYaml)
-		if err != nil {
-			log.Fatal(err)
-		}
+		stateBytes := marshal(testYaml)
 
-		_, err = MigrateV0toV1(&stateBytes)
+		_, err := MigrateV0toV1(&stateBytes)
 		if err != nil {
 			if !strings.Contains(err.Error(), "unable to migrate state file to schemaVersion 1: schemaVersion is 5") {
 				log.Fatal("Migration failed")
 			}
 		}
+	})
+}
+
+func testSameSchemaVersion(t *testing.T) {
+	t.Run("Update same schemaVersion", func(t *testing.T) {
+		testYaml := StateV0toV1{
+			SchemaVersion: 1,
+		}
+		stateBytes := marshal(testYaml)
+
+		migratedBytes, err := MigrateV0toV1(&stateBytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		decode(migratedBytes)
 	})
 }
